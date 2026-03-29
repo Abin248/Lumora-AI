3e5784538c9c1b5fdf958925d9070885420507aa
Abin248 <abinraj248@gmail.com>
2026-03-29 13:34:50 +0530

    Fix CORS URL format for frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,7 @@ const app = express();
 const corsOptions = {
     origin: [
         "http://localhost:5173", 
-        "https://lumoraai-frontend.vercel.app/" // later replace
+        "https://lumoraai-frontend.vercel.app" // later replace
     ],
     credentials: true
 };
